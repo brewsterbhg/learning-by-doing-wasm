e85d6ed855b7aa55f2b57f5ca719e257401c6645
Keith Brewster <keith.brewster22@gmail.com>
2019-06-25 11:13:45 -0400

    feat(tetris): add tetris cpp game files

---
+++ lib/c++/tetris/constants.h
@@ -0,0 +1,15 @@
+#ifndef TETRIS_CONSTANTS_H
+#define TETRIS_CONSTANTS_H
+
+namespace Constants
+{
+const int NUM_ROWS = 20;
+const int NUM_COLUMNS = 10;
+const int GAME_HEIGHT = 720;
+const int GAME_WIDTH = 360;
+const int OFFSET = GAME_WIDTH / NUM_COLUMNS;
+const int TETRINIMO_SIZE = 4;
+const int SCREEN_HEIGHT = GAME_HEIGHT + 50;
+} // namespace Constants
+
+#endif // TETRIS_CONSTANTS_H
